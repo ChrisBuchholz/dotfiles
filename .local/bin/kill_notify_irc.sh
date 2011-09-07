296c371b478e5f7efa62bca4700a94385b3d2dbd
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-09-07 21:55:48 +0200

    updated -irssi-over-ssh- scripts with a kill script for the notifications and to reattach a remote screen ssh, where irssi is running, instead of just sshing and then having to manually reattaching the screen

---
+++ .local/bin/kill_notify_irc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkill -f '.*ssh cb@178.79.133.30 -p 443 -o ServerAliveInterval=31536000'
--- .local/bin/notify_irc.sh
+++ .local/bin/notify_irc.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# irssi notifications
-
-pkill -f '.*ssh cb@178.79.133.30 -p 443 -o ServerAliveInterval=31536000'
+~/.local/bin/kill_notify_irc.sh
 
 (ssh cb@178.79.133.30 -p 443 -o ServerAliveInterval=31536000 PermitLocalCommand=no ": > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading placebo message; do
     if [ `uname` == 'Darwin' ]; then
