7fe4d9c15a2dfb19a73451dca2250314b1644333
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-11-09 19:05:13 +0100

    correct syntax

--- local/bin/notify_irc.sh
+++ local/bin/notify_irc.sh
@@ -3,10 +3,9 @@
 ~/.local/bin/kill_notify_irc.sh
 
 (ssh scootserv -p 443 -o ServerAliveInterval=31536000 PermitLocalCommand=no ": > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading placebo message; do
-    if [ `uname` == 'Darwin' ]; then
-        # dont use notifcations on mac os x
-        #growlnotify -t "${heading}" -m "${message}";
-    else
+    if [ `uname` == 'Linux' ]; then
         notify-send --hint=int:transient:1 "${heading}" "${message}"
+    #else
+        #growlnotify -t "${heading}" -m "${message}";
     fi
 done)&
