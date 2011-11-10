4bafa2c73f1774ea84eafc74a0cd77fde1f47ab5
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2011-11-10 16:57:50 +0100

    only run the ssh commands on linux

--- local/bin/notify_irc.sh
+++ local/bin/notify_irc.sh
@@ -2,10 +2,8 @@
 
 ~/.local/bin/kill_notify_irc.sh
 
-(ssh scootserv -p 443 -o ServerAliveInterval=31536000 PermitLocalCommand=no ": > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading placebo message; do
-    if [ `uname` == 'Linux' ]; then
+if [ `uname` == 'Linux' ]; then
+    (ssh scootserv -p 443 -o ServerAliveInterval=31536000 PermitLocalCommand=no ": > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading placebo message; do
         notify-send --hint=int:transient:1 "${heading}" "${message}"
-    #else
-        #growlnotify -t "${heading}" -m "${message}";
-    fi
-done)&
+    done)&
+fi
