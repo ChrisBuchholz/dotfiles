3e101a4a47aac6dc89fe6d68be2c10e74fcef680
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2012-10-16 15:23:26 +0200

    ircs now using mosh!

--- local/bin/notify_irc.sh
+++ local/bin/notify_irc.sh
@@ -3,9 +3,6 @@
 ~/.local/bin/kill_notify_irc.sh
 
 (ssh scootserv -p 443 -o ServerAliveInterval=31536000 PermitLocalCommand=no ": > .irssi/fnotify ; tail -f .irssi/fnotify " | while read heading placebo message; do
-    if [ `uname` == 'Linux' ]; then
-        notify-send --hint=int:transient:1 "${heading}" "${message}"
-    elif [ `uname` == 'Darwin' ]; then
-        /Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier IRC "${heading}" "${message}" com.googlecode.iterm2 > /dev/null 2>&1
-    fi
+    /Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier IRC "${heading}" "${message}" com.googlecode.iterm2 > /dev/null 2>&1
 done)&
+
